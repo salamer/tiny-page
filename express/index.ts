ace72b1edf7368fe6bb3e8db7dcaa698f421ae47
salamer <salamer_gaga@163.com>
2023-07-12 22:04:05 +0800

    update: fix k8s in cluster problem

--- express/index.ts
+++ express/index.ts
@@ -14,6 +14,10 @@ app.get("/app", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
+app.get("hello", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server, hello leapcell.io");
+});
+
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
